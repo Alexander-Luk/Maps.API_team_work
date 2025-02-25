4a5feb843fe36c2d0c6cf68f9eee88d725d95621
MakarHub <makar.komissarov.97@gmail.com>
2025-02-25 17:44:25 +0300

    первый вариант, тестовый

---
+++ mainpr.py
@@ -0,0 +1,62 @@
+import os
+import sys
+
+import requests
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QVBoxLayout
+
+SCREEN_SIZE = [600, 450]
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.getImage()
+        self.initUI()
+
+    def getImage(self):
+        server_address = 'https://static-maps.yandex.ru/v1?'
+        api_key = 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
+        self.first_coord = float(input('Введите первую координату: ')) # 37.530887
+        self.second_coord = float(input('Введите вторую координату: ')) # 55.703118
+
+        self.mstb = float(input('Введите масштаб(значение, приблеженное к 0.005): '))  # 37.530887
+
+        ll_spn = f'll={self.first_coord},{self.second_coord}&spn={self.mstb},{self.mstb}'
+        # Готовим запрос.
+
+        map_request = f"{server_address}{ll_spn}&apikey={api_key}"
+        response = requests.get(map_request)
+
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(map_request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
+
+        # Запишем полученное изображение в файл.
+        self.map_file = "map.png"
+        with open(self.map_file, "wb") as file:
+            file.write(response.content)
+
+    def initUI(self):
+        self.setGeometry(100, 100, *SCREEN_SIZE)
+        self.setWindowTitle('Отображение карты')
+
+        ## Изображение
+        self.pixmap = QPixmap(self.map_file)
+        self.image = QLabel(self)
+        self.image.move(0, 0)
+        self.image.resize(600, 450)
+        self.image.setPixmap(self.pixmap)
+
+    def closeEvent(self, event):
+        """При закрытии формы подчищаем за собой"""
+        os.remove(self.map_file)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
